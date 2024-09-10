1be74acdcac8b3682c49ab45d7bd3fb89d6ee64f
ramakishorenooji <kishore88.n@gmail.com>
2024-09-10 18:14:37 +0530

    postgres address updated and changed to env variable

--- employee_app/config.py
+++ employee_app/config.py
@@ -9,7 +9,6 @@ class Config:
 class DevelopmentConfig(Config):
     """Configuration for development environment."""
     DEBUG = True
-    print("--- >>", DB_PASSWORD)
     SQLALCHEMY_DATABASE_URI = f"postgres://avnadmin:{DB_PASSWORD}@pg-2eade052-kishore88-20d6.c.aivencloud.com:14147/defaultdb?sslmode=require"  # Your development database URI
 
 class TestingConfig(Config):

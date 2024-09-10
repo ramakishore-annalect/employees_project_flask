e028033237a4100196bdf9906cec97f8eb26e046
ramakishorenooji <kishore88.n@gmail.com>
2024-09-10 18:15:27 +0530

    postgres address updated and changed to env variable

--- employee_app/config.py
+++ employee_app/config.py
@@ -1,6 +1,3 @@
-import os
-
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')  # 'default_password' is a fallback value
 
 class Config:
     """Base configuration class."""
@@ -9,9 +6,9 @@ class Config:
 class DevelopmentConfig(Config):
     """Configuration for development environment."""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"postgres://avnadmin:{DB_PASSWORD}@pg-2eade052-kishore88-20d6.c.aivencloud.com:14147/defaultdb?sslmode=require"  # Your development database URI
+    SQLALCHEMY_DATABASE_URI = "postgresql://ramakishorenooji:@localhost/testdb"  # Your development database URI
 
 class TestingConfig(Config):
     """Configuration for testing environment."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = f"postgres://avnadmin:{DB_PASSWORD}@pg-2eade052-kishore88-20d6.c.aivencloud.com:14147/defaultdb?sslmode=require"  # Your testing database URI
+    SQLALCHEMY_DATABASE_URI = "postgresql://ramakishorenooji:@localhost/mock_db"  # Your testing database URI

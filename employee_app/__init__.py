8f7fc77ddffd4e037a02777f541a1b1cbdd29ae3
ramakishorenooji <kishore88.n@gmail.com>
2024-09-10 19:55:27 +0530

    updated the password passing method

--- employee_app/__init__.py
+++ employee_app/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password') 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://avnadmin:{DB_PASSWORD}@pg-2eade052-kishore88-20d6.c.aivencloud.com:14147/defaultdb?sslmode=require"  # Default for development
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://avnadmin:{DB_PASSWORD}@pg-2eade052-kishore88-20d6.c.aivencloud.com:14147/defaultdb?sslmode=require"  # Default for development
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()  # Initialize without the app
